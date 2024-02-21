9299285d69762c97a2e38a03642c4e182165649d
fares meier <fares.meier@hotmail.com>
2024-02-21 12:49:29 +0100

    restaurant controller action and views completed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  VALID_CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
   validates :name, :address, :category, :phone_number, presence: true
+  validates :category, inclusion: { in: VALID_CATEGORIES, message: '%<value>s is not a valid category' }
 end
